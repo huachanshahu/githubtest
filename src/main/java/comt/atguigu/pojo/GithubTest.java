60b742d2a3f93682e97671c3d35c22fb1e4876cc
殷俊鹏 <506628473@qq.com>
2020-09-26 23:50:38 +0800

    测试github

---
+++ src/main/java/comt/atguigu/pojo/GithubTest.java
@@ -0,0 +1,15 @@
+package comt.atguigu.pojo;
+
+/**
+ * @author yjp
+ * @create 2020-09-26 23:44
+ */
+public class GithubTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("测试");
+
+    }
+
+}
